cf5b16e70a33fece7b6d779da84ed84d9be81f9c
Lorenzo Andreoli <andreoli95@gmail.com>
2024-10-24 01:03:16 +0200

    feat: add text from query param

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
-import './App.css';
-import {useState} from "react";
+import "./App.css";
+import { useEffect, useState } from "react";
 import parse from "./Parser";
 import JsonView from "react18-json-view";
 
 function App() {
+  const params = new URLSearchParams(window.location.search);
 
-    const [value, setValue] = useState("");
+  const [value, setValue] = useState(params.get("v") || "");
 
-    const handleChange = event => {
-        const res = parse(event.target.value);
-        setValue(res);
-    };
+  useEffect(() => {
+    if (params.get("v")) {
+      const res = parse(params.get("v"));
+      setValue(res);
+    }
+  }, [value]);
 
-    return (
-        <div className={"container"}>
-            <div>
-                <h1>Bear 🐻 Log</h1>
-            </div>
-            <div className={"main-container"}>
-                <div className={"left-container"}>
-                    <textarea
-                        type="text"
-                        id="message"
-                        onChange={handleChange}
-                        className={"textarea"}
-                        placeholder={"Paste your log here"}
-                    />
-                </div>
-                <div className={"right-container"}>
-                    <div className={"right-content"}>
-                        <JsonView src={value}/>
-                    </div>
-                </div>
-            </div>
+  const handleChange = (event) => {
+    const res = parse(event.target.value);
+    setValue(res);
+  };
+
+  return (
+    <div className={"container"}>
+      <div>
+        <h1>Bear 🐻 Log</h1>
+      </div>
+      <div className={"main-container"}>
+        <div className={"left-container"}>
+          <textarea
+            type="text"
+            id="message"
+            onChange={handleChange}
+            className={"textarea"}
+            defaultValue={value}
+            placeholder={"Paste your log here"}
+          />
         </div>
-    );
+        <div className={"right-container"}>
+          <div className={"right-content"}>
+            <JsonView src={value} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
